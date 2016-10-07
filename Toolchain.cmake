2b176fa3ea15233656bcbb6e311731ee470ac116
Huu Nguyen <whoshuu@gmail.com>
2016-10-07 11:22:12 -0700

    Add Toolchain.cmake file

---
+++ Toolchain.cmake
@@ -0,0 +1,23 @@
+set(_target_name mipsel-linux-android)
+set(_target_root ${CMAKE_CURRENT_LIST_DIR}/${_target_name})
+
+set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS} -march=mips32r2 -O2")
+set(CMAKE_CXX_FLAGS_INIT ${CMAKE_C_FLAGS})
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static -static-libgcc -static-libstdc++")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(BUILD_SHARED_LIBS OFF)
+
+set(CMAKE_C_COMPILER ${_target_root}/bin/${_target_name}-gcc)
+set(CMAKE_CXX_COMPILER ${_target_root}/bin/${_target_name}-g++)
+set(CMAKE_NM ${_target_root}/bin/${_target_name}-nm)
+set(CMAKE_AR ${_target_root}/bin/${_target_name}-ar)
+set(CMAKE_STRIP ${_target_root}/bin/${_target_name}-strip)
+set(CMAKE_RANLIB ${_target_root}/bin/${_target_name}-ranlib)
+set(CMAKE_OBJDUMP ${_target_root}/bin/${_target_name}-objdump)
+set(CMAKE_OBJCOPY ${_target_root}/bin/${_target_name}-objcopy)
+set(CMAKE_LINKER ${_target_root}/bin/${_target_name}-ld)
+
+if(CCACHE)
+    set(CMAKE_C_COMPILER ${CCACHE} ${CMAKE_C_COMPILER})
+    set(CMAKE_CXX_COMPILER ${CCACHE} ${CMAKE_CXX_COMPILER})
+endif()
